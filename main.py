2b3792492937e3460c52c9252f3319a44741355e
Anastasiya Samokhina <anastasiya0625@gmail.com>
2024-02-15 16:04:36 +0300

    task3

--- main.py
+++ main.py
@@ -5,12 +5,22 @@ class Category:
     def __init__(self, name: str, description: str):
         self.name = name
         self.description = description
-        self.products = []
+        self.__products = []  # делаем список приватным
         Category.total_categories += 1
 
     def add_product(self, product):
-        self.products.append(product)
-        Category.total_unique_products.add(product.name)
+        self.__products.append(product)
+
+    def get_products(self):
+        products_info = ""
+        for product in self.__products:
+            products_info += f"{product}, 80 руб. Остаток: 15 шт.\n"
+
+category = Category("Электроника", "Товары электроники")
+category.add_product("Смартфон")
+category.add_product("Ноутбук")
+print(category.get_products())
+
 
 class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
@@ -19,6 +29,10 @@ class Product:
         self.price = price
         self.quantity = quantity
 
+    @classmethod
+    def create_product(cls, name: str, description: str, price: float, quantity: int):
+        return cls(name, description, price, quantity)
 
-
+product1 = Product.create_product("Смартфон", "Мощный смартфон", 500, 10)
+product2 = Product.create_product("Ноутбук", "Легкий и компактный ноутбук", 800, 5)
 
